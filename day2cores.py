9731ace5d71cc26ec03e25d21e603873062e8a28
Rojiiiiina <rojinamishra35@gmail.com>
2026-06-02 16:20:31 +0545

    day 2 function

---
+++ day2cores.py
@@ -0,0 +1,82 @@
+#Python functions
+# def my_function():
+#     print("This is my function")
+#     print("Bye Bye Rojina")
+#     print("Hi Hi Dude")
+
+# my_function()
+# my_function()
+# my_function()
+
+# Arguments
+# def my_function(fname): #(this is parameter)
+#     print(fname + " Mishra")
+    
+# my_function("Rojina")
+# my_function("Luna")    #(this is argument)
+# my_function("Rhythm")
+
+
+# using default value
+# def my_function(name = "roji"):
+#     print("Hello", name)
+
+# my_function("Nischal")    
+# my_function()
+# my_function("Nikita")
+ 
+
+# def my_function(animal, name):
+#     print("I have a" , animal)
+#     print("His name is", name)
+
+# my_function("dog", "Luffy")    
+
+# def my_function(*kids):
+#     print("The youngest one is " + kids[4])
+
+# my_function("Luna", "Rojina", "Rhythm", "Fidam", "Sanskriti")
+   
+# def my_function(*number):
+#     square = []
+#     for num in number:
+#         square.append(num**2)
+#     return square
+
+# print (my_function(2, 3, 4))
+
+# def my_function(*number):
+#     total = []
+#     for num in number:
+#         total.append(num > 10 )
+#     return total
+# print(my_function(2, 50, 20))
+
+# def my_function(*numbers):
+#     sum = 0
+#     for num in numbers:
+#         sum+=num
+#     return sum
+# print(my_function(1,2,3,4,5))
+
+
+# def my_function(*numbers):
+#     multiply = 1
+#     for num in numbers:
+#         multiply= multiply*num
+#     return multiply
+# print(my_function(4,7,10))
+
+
+# def my_function(*number):
+#     total = 0
+#     for num in number:
+#         total+=1
+#     return total
+# print(my_function(1,2,3,4,5,6,7))
+
+
+
+    
+            
+        
